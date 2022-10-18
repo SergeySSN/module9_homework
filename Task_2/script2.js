c6fac4914c4ddf0bc8db956896e072bd44ff388d
Sergei <112326030+SergeySSN@users.noreply.github.com>
2022-10-18 14:13:03 +0400

    Add files via upload

---
+++ Task_2/script2.js
@@ -0,0 +1,60 @@
+/*
+Вам дана заготовка и результат, который вы должны получить. Ваша задача — написать код, который будет преобразовывать JSON в JS-объект и выводить его в консоль.
+
+JSON:
+
+{
+ "list": [
+  {
+   "name": "Petr",
+   "age": "20",
+   "prof": "mechanic"
+  },
+  {
+   "name": "Vova",
+   "age": "60",
+   "prof": "pilot"
+  }
+ ]
+}
+JS-объект:
+
+{
+  list: [
+    { name: 'Petr', age: 20, prof: 'mechanic' },
+    { name: 'Vova', age: 60, prof: 'pilot' },
+  ]
+} */
+
+const myJsonString = `
+{
+"list": [
+  {
+   "name": "Petr",
+   "age": "20",
+   "prof": "mechanic"
+  },
+  {
+   "name": "Vova",
+   "age": "60",
+   "prof": "pilot"
+  }
+ ]
+}
+`
+
+const data = JSON.parse (myJsonString);
+
+const myList = data.list;
+
+const list = [];
+myList.forEach (member => {
+const result = {
+  name: member.name,
+  age: Number(member.age),
+  prof: member.prof
+}
+//console.log (result)
+list.push (result);  
+})
+console.log ('list: ', list);
